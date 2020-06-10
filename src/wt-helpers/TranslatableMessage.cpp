24615407d3641e59159a49685fdfb435141d09e2
Captain Crutches <captaincrutches@gmail.com>
2020-06-10 17:45:29 -0400

    Add TranslatableMessage from solvernia

---
+++ src/wt-helpers/TranslatableMessage.cpp
@@ -0,0 +1,59 @@
+#include <Wt/Json/Array.h>
+#include <Wt/Json/Object.h>
+#include <Wt/Json/Value.h>
+#include <Wt/WString.h>
+
+#include <string>
+#include <string_view>
+
+#include "TranslatableMessage.h"
+
+TranslatableMessage::TranslatableMessage(std::string_view message, const bool needsTranslating)
+  : message{message},
+    needsTranslating{needsTranslating}
+{}
+
+TranslatableMessage::TranslatableMessage(const Wt::Json::Object& jsonObj)
+  : message{jsonObj.get(KEY_MESSAGE).toString().orIfNull("")},
+    needsTranslating{jsonObj.get(KEY_NEEDSTRANSLATING).toBool().orIfNull(false)}
+{
+    // Take the "args" array from the object and pull our args off it
+    auto& rawArgs = jsonObj.get(KEY_ARGS).orIfNull(Wt::Json::Array());
+    args.reserve(rawArgs.size());
+    for (const auto& value : rawArgs)
+    {
+        // We expect the value to be an object we can use to build another TranslatableMessage
+        const auto& obj = value.orIfNull(Wt::Json::Object({}));
+        args.emplace_back(obj);
+    }
+}
+
+void TranslatableMessage::addArg(const TranslatableMessage& arg)
+{
+    args.push_back(arg);
+}
+
+Wt::WString TranslatableMessage::translate() const
+{
+    if (!needsTranslating)
+        return message;
+
+    auto translated = Wt::WString::tr(message);
+    for (auto& arg : args)
+        translated.arg(arg.translate());
+    return translated;
+}
+
+Wt::Json::Object TranslatableMessage::toJson() const
+{
+    // Build out json objects for all our children
+    Wt::Json::Array jsonArgs;
+    for (const auto& child : args)
+        jsonArgs.push_back(Wt::Json::Value(child.toJson()));
+
+    return {
+        { KEY_MESSAGE, Wt::Json::Value(message) },
+        { KEY_NEEDSTRANSLATING, Wt::Json::Value(needsTranslating) },
+        { KEY_ARGS, Wt::Json::Value(jsonArgs) }
+    };
+}
---
+++ src/wt-helpers/TranslatableMessage.h
@@ -0,0 +1,49 @@
+#ifndef TRANSLATABLEMESSAGE_H
+#define TRANSLATABLEMESSAGE_H
+
+#include <Wt/WString.h>
+#include <Wt/Json/Object.h>
+
+#include <string>
+#include <string_view>
+#include <vector>
+
+/**
+ * A tree-like structure of messages that might need to be translated,
+ * with arguments which themselves may need to be translated.
+ *
+ * I don't expect one of these will ever have more than 1 sub-level,
+ * but it's useful for storing raw info that can be turned into a localized string.
+ *
+ * Converts back and forth to a json object that looks like:
+ * {
+ *      "message": string,
+ *      "needsTranslating": boolean,
+ *      "args": TranslatableMessage[]
+ * }
+ *
+ * The idea being that you can translate a key like "${user} was logged out (${reason})"
+ * where the root key has args, one arg needs translating and one does not.
+ */
+class TranslatableMessage
+{
+public:
+    TranslatableMessage(std::string_view message, const bool needsTranslating = false);
+    TranslatableMessage(const Wt::Json::Object& jsonObj);
+
+    void addArg(const TranslatableMessage& arg);
+
+    Wt::WString translate() const;
+    Wt::Json::Object toJson() const;
+
+    static constexpr const char* KEY_MESSAGE{"message"};
+    static constexpr const char* KEY_NEEDSTRANSLATING{"needsTranslating"};
+    static constexpr const char* KEY_ARGS{"args"};
+
+private:
+    std::string message;
+    bool needsTranslating;
+    std::vector<TranslatableMessage> args;
+};
+
+#endif  // TRANSLATABLEMESSAGE_H
